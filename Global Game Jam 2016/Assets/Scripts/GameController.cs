78b04df8c924c7770b657b5bd28eb681869572dc
seanManY <maguire.sean037@gmail.com>
2016-01-30 06:58:29 -0800

    merg

--- Global Game Jam 2016/Assets/Scripts/GameController.cs
+++ Global Game Jam 2016/Assets/Scripts/GameController.cs
@@ -6,25 +6,20 @@ public class GameController : MonoBehaviour {
     // Instance Variables
     public GameObject floor;
     public GameObject player;
-<<<<<<< HEAD
+
     public int rate = 10;
     private int time;
 
-	// Use this for initialization
-	void Start ()
-    {
-        time = rate;
-        
-=======
     public GameObject power;
 
 	// Use this for initialization
 	void Start () 
     {
+        time = rate;
+
         Vector3 powerPos = new Vector3(-5, 5, 0);
         Instantiate(power, powerPos, Quaternion.identity);
 
->>>>>>> origin/master
         Vector3 playerPos = new Vector3(-5, 5, 0);
         Instantiate(player, playerPos, Quaternion.identity);
 
