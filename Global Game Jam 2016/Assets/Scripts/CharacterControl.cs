5abe23fc5506dea047a2c9369ce2c09047b93ca7
seanManY <maguire.sean037@gmail.com>
2016-01-30 01:37:08 -0800

    CharacterControl

---
+++ Global Game Jam 2016/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControl : MonoBehaviour {
+
+    public enum State
+    {
+        normal,
+        jumping
+    }
+
+    public int   jumpHeight = 5;
+    public State state;
+
+    
+    // rigidbody.constraints = RigidBodyConstraints.FreezePositionY;
+	// Use this for initialization
+	void Start () 
+    {
+        state = State.normal;
+	}
+	
+	//Update is called once per frame
+	void Update () 
+    {
+       
+       
+        if (Input.GetKeyDown("space"))
+            Jump();
+	}
+
+    void Jump()
+    {
+        if (state == State.normal)
+        {
+            GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity + new Vector3(0, jumpHeight, 0);
+            state = State.jumping;
+        }
+    }
+
+    //checks if player is touching the floor
+    void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.tag == "floor" )
+        {
+           
+            state = State.normal;
+        }
+     }
+}
