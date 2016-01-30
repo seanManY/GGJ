a601a83dd5abbbb3c5fa1bd2176f89ad54494de6
reSLANE <sml37s@yahoo.com>
2016-01-30 01:38:43 -0800

    Power 1

---
+++ Global Game Jam 2016/Assets/Scrpts/Power.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Power : MonoBehaviour
+{
+    // Instance Variables
+    public int speed;
+    public int life;
+    private int type;
+    private int posi;
+
+    // Use this for initialization
+    void Start(int type, int posi)
+    {
+        this.type = type;
+        transform.Translate(0, posi, 0);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+        transform.Translate(Time.deltaTime * -speed, 0, 0);
+
+        life--;
+        if(life <= 0)
+            Destroy(this.gameObject);
+    }
+    
+    // Methods
+    void OnCollisionEnter(Collider obj)
+    {
+        if(obj.tag == "player")
+            Destroy(this.gameObject);
+    }
+
+    public int Type()
+    {
+        return type;
+    }
+}
