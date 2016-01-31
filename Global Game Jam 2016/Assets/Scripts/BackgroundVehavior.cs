c44a62870be6d71fe97f6af8830e36ab9a07e14c
seanManY <maguire.sean037@gmail.com>
2016-01-30 19:04:15 -0800

    Basic Textures

---
+++ Global Game Jam 2016/Assets/Scripts/BackgroundVehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundVehavior : MonoBehaviour {
+
+    public int speed = 5;
+    public int life = 100;
+
+ 
+
+    void FixedUpdate()
+    {
+        transform.Translate(Time.deltaTime * -speed, 0, 0);
+
+        life--;
+        if (life <= 0)
+            Destroy(this.gameObject);
+    }
+}
--- Global Game Jam 2016/Assets/Scripts/GameController.cs
+++ Global Game Jam 2016/Assets/Scripts/GameController.cs
@@ -15,19 +15,37 @@ public class GameController : MonoBehaviour {
     public int rate = 10;
     private int time;
 
+    public int wRate = 20;
+    private int wTime;
+
     private GameObject POWER;
     public  GameObject power;
+
+    public GameObject background;
+
+    private bool block;
+    private GameObject last;
+
+    public float ranRate;
+    private float ranTime;
 
 	// Use this for initialization
 	void Start () 
     {
         time = rate;
-
+        ranTime = ranRate;
+        block = true;
+        //background
+        for (int i = -1; i < 3; i++)
+        {
+            Vector3 backGroundPos = new Vector3(32 * i, .5f * i, -3);
+            Instantiate(background, backGroundPos, Quaternion.identity);
+        }
         
 
         //player
         Vector3 playerPos = new Vector3(-5, 5, 0);
-        Instantiate(player, playerPos, Quaternion.identity);
+        Instantiate(player, playerPos, Quaternion.Euler(-90, 0, 0));
 
         for (int i = 0; i < 50; i++)
         {
@@ -40,35 +58,67 @@ public class GameController : MonoBehaviour {
     // Update is called once per frame
     void FixedUpdate() 
     {
-        random = Random.Range(0, 10);
+        random = Random.Range(0, 100);
         time--;
         if (time < 0)
         {
+            
             //floor generator
             Vector3 floorPos = new Vector3(15, 0, 0);
             Instantiate(floor, floorPos, Quaternion.identity);
+
+
             time = rate;
-
-            if(random > 5)
+            ranTime--;
+            if (ranTime < 0)
             {
-                //obstacle generator
-                //Vector3 obstaclePos = new Vector3(15, 0, 0);
-                //Instantiate(obstacle, obstaclePos, Quaternion.Euler(0, 0, 0));
 
-                //spider generator
-                //Vector3 spiderPos = new Vector3(15, 7, 0);
-                //Instantiate(spider, spiderPos, Quaternion.Euler(0, 0, 0));
-
-                //power ups
-                int powRandHeight = Random.Range(2, maxRange);
-                Vector3 powerPos = new Vector3(15, powRandHeight, 0);
-                POWER = (GameObject)Instantiate(power, powerPos, Quaternion.identity);
-                int powRandom = Random.Range(0, 3);
-                POWER.GetComponent<Power>().setTag(powRandom);
-                
-                
+                if (random <= 20)
+                {
+                    //power ups
+                    int powRandHeight = Random.Range(2, maxRange);
+                    Vector3 powerPos = new Vector3(15, powRandHeight, 0);
+                    POWER = (GameObject)Instantiate(power, powerPos, Quaternion.identity);
+                    int powRandom = Random.Range(0, 3);
+                    POWER.GetComponent<Power>().setTag(powRandom);
+                    block = false;
+                    ranTime = ranRate;
+                }
+                //obtacle, spikes
+                else if (random <= 55)
+                {
+                    //obstacle generator
+                    Vector3 obstaclePos = new Vector3(15, 0, 0);
+                    Instantiate(obstacle, obstaclePos, Quaternion.Euler(0, 0, 0));
+                    block = false;
+                    ranTime = ranRate;
+                }
+                //spider
+                else if (random <= 90)
+                {
+                    //spider generator
+                    Vector3 spiderPos = new Vector3(15, 7, 0);
+                    Instantiate(spider, spiderPos, Quaternion.Euler(0, 0, 0));
+                    block = false;
+                    ranTime = ranRate;
+                }
             }
+            
+
+            wTime--;
+            if(wTime < 0)
+            {
+                Vector3 backGroundPos = new Vector3(40 , .5f, -3);
+                Instantiate(background, backGroundPos, Quaternion.identity);
+
+                wTime = wRate;
+            }
+
+            block = true;
+            //ranRate *= .00001f;
+            Debug.Log(ranRate);
            
         }
+
 	}
 }
