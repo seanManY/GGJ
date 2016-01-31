2ab4af9d8ca3c3fd4664b5e0cc6be52b7aa99c92
reSLANE <sml37s@yahoo.com>
2016-01-31 00:39:16 -0800

    HUD!!

--- Global Game Jam 2016/Assets/Scripts/GUI.cs
+++ Global Game Jam 2016/Assets/Scripts/GUI.cs
@@ -5,10 +5,13 @@ public class GUI : MonoBehaviour
 {
 
     // Instance Variables
-    public GameObject Outline;
+    public GameObject Background;
     public GameObject Red;
     public GameObject Green;
     public GameObject Blue;
+    public GameObject RedOut;
+    public GameObject GreenOut;
+    public GameObject BlueOut;
     public GameObject Heart;
     private GameObject[,] Powers = new GameObject[3,3];
     private GameObject[] Health = new GameObject[3];
@@ -17,18 +20,31 @@ public class GUI : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        guiPos = new Vector3(-11, 8, -1);
+        Instantiate(Background, new Vector3(-7.25f, 4, -1), Quaternion.identity);
+
+        guiPos = new Vector3(-6.81f, 3, -2.5f);
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
             {
-                Powers[i,j] = (GameObject)Instantiate(Outline, guiPos + new Vector3(2*i, -j, 0), Quaternion.identity);
+                switch (j)
+                {
+                    case 0:
+                        Powers[i, j] = (GameObject)Instantiate(RedOut, guiPos + new Vector3(.75f * (float)i, -j, 0), Quaternion.identity);
+                        break;
+                    case 1:
+                        Powers[i, j] = (GameObject)Instantiate(GreenOut, guiPos + new Vector3(.75f * (float)i, -j, 0), Quaternion.identity);
+                        break;
+                    case 2:
+                        Powers[i, j] = (GameObject)Instantiate(BlueOut, guiPos + new Vector3(.75f * (float)i, -j, 0), Quaternion.identity);
+                        break;
+                }
             }
         }
 
         for (int i = 0; i < 3; i++)
         {
-            Health[i] = (GameObject)Instantiate(Heart, new Vector3(10 - 2*i, 8, -1), Quaternion.identity);
+            Health[i] = (GameObject)Instantiate(Heart, new Vector3(-7.45f + i, 7, -2), Quaternion.identity);
         }
     }
 	
@@ -44,13 +60,13 @@ public class GUI : MonoBehaviour
         switch(y)
         {
             case 0:
-                Powers[x,y] = (GameObject)Instantiate(Red, guiPos + new Vector3(2 * x, -y, 0), Quaternion.identity);
+                Powers[x,y] = (GameObject)Instantiate(Red, guiPos + new Vector3(.75f * (float)x, -y, 0), Quaternion.identity);
                 break;
             case 1:
-                Powers[x,y] = (GameObject)Instantiate(Green, guiPos + new Vector3(2 * x, -y, 0), Quaternion.identity);
+                Powers[x,y] = (GameObject)Instantiate(Green, guiPos + new Vector3(.75f * (float)x, -y, 0), Quaternion.identity);
                 break;
             case 2:
-                Powers[x,y] = (GameObject)Instantiate(Blue, guiPos + new Vector3(2 * x, -y, 0), Quaternion.identity);
+                Powers[x,y] = (GameObject)Instantiate(Blue, guiPos + new Vector3(.75f * (float)x, -y, 0), Quaternion.identity);
                 break;
         }
     }
